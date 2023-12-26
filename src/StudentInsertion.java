8b7d2f49192ea9ece5abc29148f77d55a1b3ed62
hridoyPRGMR <hridoymia.prgmr@gmail.com>
2023-12-26 22:49:26 +0600

    handling exception in studentInsertion

--- src/StudentInsertion.java
+++ src/StudentInsertion.java
@@ -3,9 +3,9 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement; // Importing java.sql.Statement instead of com.mysql.cj.xdevapi.Statement
+import java.sql.Statement;
 
-class StudentInsertion {
+public class StudentInsertion {
     public void studentInsert(Connection con, Statement stmt, BufferedReader in) {
         try {
             System.out.println("Enter number of rows: ");
@@ -14,22 +14,29 @@ class StudentInsertion {
             String sql = "INSERT INTO student(name, class) VALUES(?, ?)";
             PreparedStatement pstmt = con.prepareStatement(sql);
 
-            System.out.println("Enter name (String) and class (int)");
-            while (n-- > 0) {
+            while (n > 0) {
+                System.out.println("Enter name (String) and class (int)");
                 String name = in.readLine();
-                int cls = Integer.parseInt(in.readLine());
+                String clas = in.readLine();
 
-                pstmt.setString(1, name);
-                pstmt.setInt(2, cls);
-                
-                pstmt.addBatch(); // Add the prepared statement to the batch for bulk execution
+                int cls;
+                try {
+                    cls = Integer.parseInt(clas);
+                    pstmt.setString(1, name);
+                    pstmt.setInt(2, cls);
+                    pstmt.addBatch(); // Add the prepared statement to the batch for bulk execution
+                    n--;
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid class format. Please enter an integer for class.");
+                    continue;
+                }
             }
-            
+
             pstmt.executeBatch(); // Execute all statements in the batch at once
             System.out.println("Data Inserted");
 
-        } catch (SQLException | NumberFormatException | IOException e) {
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
-        } 
+        }
     }
 }
